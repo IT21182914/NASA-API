6643480b7c2c3b30954c5c8d7693c6cf99ce1f99
Dilan Shanuka <99383107+IT21182914@users.noreply.github.com>
2024-05-05 15:47:26 +0530

    Header

--- frontend/src/components/Layout/Header.js
+++ frontend/src/components/Layout/Header.js
@@ -12,10 +12,14 @@ const Header = () => {
   };
 
   return (
-    <header className="bg-blue-500 py-4">
+    <header className="bg-blue-500 py-4 ">
+      {" "}
+      {/* Added font-mono class */}
       <div className="container mx-auto flex flex-col md:flex-row justify-between items-center px-4">
         <div className="flex items-center mb-4 md:mb-0">
-          <h1 className="text-white text-3xl font-bold">NASA Dashboard</h1>
+          <h1 className="text-white text-3xl font-bold font-sans">
+            NASA Dashboard
+          </h1>
         </div>
         <nav className="md:ml-4">
           <ul className="flex flex-col md:flex-row">
